e1ebd3806149cdf73c6ed2051532e0b679949535
Herbali Henda <derbalihenda93@gmail.com>
2023-11-11 13:57:01 +0100

    new app

--- Assignments/React/First React With JSX/create-react-app/src/App.js
+++ Assignments/React/First React With JSX/create-react-app/src/App.js
@@ -7,11 +7,12 @@ function App() {
       <legend>App.js</legend>
 
       <h1> Hello Dojo! </h1>
-      <h2> Things I need to do : </h2>
+      <ul>
         <li>Learn React</li>
         <li>Climb Mt.Everest</li>
         <li>Run a marathon</li>
         <li>Feed the dogs</li>
+      </ul>
       
 
     </fieldset>
