67599058c7aebd5f195c956106c9967dc6e78605
Herbali Henda <derbalihenda93@gmail.com>
2023-11-11 13:50:46 +0100

    new commit

---
+++ Assignments/React/First React With JSX/create-react-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <fieldset>
+      <legend>App.js</legend>
+
+      <h1> Hello Dojo! </h1>
+      <h2> Things I need to do : </h2>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      
+
+    </fieldset>
+  );
+}
+
+export default App;
